9166086d2b3cc6ab9b6d83342246e7afb88bad61
JossanTorner <josefintorner@gmail.com>
2025-02-26 14:58:31 +0100

    one done

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,9 @@
 import java.io.File
+import java.util.IntSummaryStatistics
+
+
+//AoC 2017 day 2.
+
 
 fun highestNumb (list : List<Int>) : Int {
     return list.max()
@@ -12,19 +17,15 @@ fun diff(max : Int, min : Int) : Int {
     return max - min
 }
 
+
 //Summera diff mellan alla min + max values
 fun sumDiff(data : List<List<Int>>) : Int {
     tailrec fun diff (data : List<List<Int>>, index : Int, sum : Int) : Int {
        if(index == data.size) return sum
-        return diff (data, index + 1, diff(highestNumb(data[index]), lowestNumber(data[index])))
+        return diff (data, index + 1, sum + diff(highestNumb(data[index]), lowestNumber(data[index])))
    }
     return diff(data, 0, 0)
 }
-
-//räkna ut max och min värde på varje rad.
-//fun maxMin(data : List<String>) : Long{
-//
-//}
 
 
 //För varje plats i lista med String, skapa en lista av ints och lägg i en lista
@@ -37,10 +38,43 @@ fun createIntLists(data : List<String>) : List<List<Int>>{
     return mainList
 }
 
+fun sumDividedIndex(data : List<List<Int>>) : Int {
+   tailrec fun divList(data: List<List<Int>>, index: Int, sum: Int): Int {
+        if (index == data.size) return sum
+        return divList(data, index + 1, sum + div(data[index]))
+    }
+    return divList(data, 0, 0)
+}
+
+
+// För varje element (behövs index), kolla om vardera element efter det är jämnt delbart, i så fall skicka vidare talet av divisionen
+// om elementet ej är delbart med nästa element, jämför med nästa (behövs index för nuvarande jämförelse)
+
+
+
+fun div(data : List<Int>) : Int{
+    tailrec fun loop(data : List<Int>, currentNumber : Int, indexComp : Int) : Int {
+        if(currentNumber == data.size - 1) return 0
+        if (indexComp == data.size) return loop(data, currentNumber + 1, 0)
+        if(data[currentNumber] % data[indexComp] == 0) return data[currentNumber] / data[indexComp]
+        return loop(data, currentNumber, indexComp +1)
+    }
+    return loop(data, 0, 0)
+}
+
+
+
+
+
+
+
+
+
+
 fun readFile() : List<String>{
     var data = mutableListOf<String>()
     try{
-        data = File("src/Data.txt").readLines().toMutableList()
+        data = File("src/data.txt").readLines().toMutableList()
     }
     catch (e: NumberFormatException){
         println("Not correct data in file")
@@ -52,7 +86,23 @@ fun readFile() : List<String>{
 }
 
 fun main() {
-    createIntLists(readFile()).forEach(::println)
-    val sum = sumDiff(createIntLists(readFile()))
+//    createIntLists(readFile()).forEach(::println)
+//    val sum = sumDiff(createIntLists(readFile()))
+//    println(sum)
+
+    val sum = sumDividedIndex(createIntLists(readFile()))
     println(sum)
 }
+
+// fun division(list: List<Int>): Int {
+//    tailrec fun findNumbers(list: List<Int>, sum : Int, index: Int, currentComparator : Int): Int {
+//        if (index >= list.size - 1) return sum
+//        if (currentComparator == list.size) return findNumbers(list, sum, index + 1, index + 2)
+//
+//        return if (list[index] % list[currentComparator] == 0) {
+//            list[index] / list[currentComparator]
+//        }
+//        else findNumbers(list, sum, index, currentComparator + 1)
+//    }
+//    return findNumbers(list, 0, 0, 1)
+//}
